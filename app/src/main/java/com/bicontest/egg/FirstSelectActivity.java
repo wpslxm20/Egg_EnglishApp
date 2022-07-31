77947d9cd9c3d531f41eb25ab613391bf2ee9c9e
jung0115 <jungim7490@gmail.com>
2022-08-01 01:50:29 +0900

    startPage

---
+++ app/src/main/java/com/bicontest/egg/FirstSelectActivity.java
@@ -0,0 +1,15 @@
+// 초기 페이지 - 관심 단어 선택
+package com.bicontest.egg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_select);
+    }
+}
