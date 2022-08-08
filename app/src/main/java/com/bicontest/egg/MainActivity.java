f503527ecd5cd9e1a8a095c5cb0be6de5b7a01b9
gogos <gosumin0710@naver.com>
2022-08-08 21:57:17 +0900

    middle push

--- app/src/main/java/com/bicontest/egg/MainActivity.java
+++ app/src/main/java/com/bicontest/egg/MainActivity.java
@@ -1,8 +1,9 @@
 package com.bicontest.egg;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Intent intent = new Intent(MainActivity.this, word.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/bicontest/egg/flash.java
@@ -0,0 +1,14 @@
+package com.bicontest.egg;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class flash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash);
+    }
+}
