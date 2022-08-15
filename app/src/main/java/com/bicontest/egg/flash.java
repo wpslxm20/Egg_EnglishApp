406819f7663c3e8471d4e67cce5036f05c37a2e1
jung0115 <jungim7490@gmail.com>
2022-08-15 14:52:52 +0900

    합치기

---
+++ app/src/main/java/com/bicontest/egg/flash.java
@@ -0,0 +1,16 @@
+package com.bicontest.egg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class flash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash);
+    }
+}
