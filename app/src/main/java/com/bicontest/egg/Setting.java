e3cf7c53743352b3dc493ad06fc064eaee95a465
sumin <gosumin0710@naver.com>
2022-08-23 23:18:19 +0900

    flash 기능 미완

--- app/src/main/java/com/bicontest/egg/Setting.java
+++ app/src/main/java/com/bicontest/egg/Setting.java
@@ -3,12 +3,41 @@ package com.bicontest.egg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Setting extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        TextView abc = (TextView)findViewById(R.id.abc);
+
+        EditText flashEdit = (EditText) findViewById(R.id.flashEdit);
+        flashEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //입력하기 전
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //EditText에 변화가 있을 때
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //입력이 끝났을 때
+                int flashsec = Integer.parseInt(flashEdit.getText().toString());
+                ((glovalVariable) getApplication()).setFlashSecond(flashsec);
+                int a = ((glovalVariable) getApplication()).getFlashSecond();
+                abc.setText(a);
+            }
+        });
     }
 }
