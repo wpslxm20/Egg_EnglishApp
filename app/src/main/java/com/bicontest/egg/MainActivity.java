633e8bbe56389189c3e67a4955e8dbbac566cd85
gogos <gosumin0710@naver.com>
2022-09-06 19:12:46 +0900

    flash 기능 보완

--- app/src/main/java/com/bicontest/egg/MainActivity.java
+++ app/src/main/java/com/bicontest/egg/MainActivity.java
@@ -2,14 +2,14 @@ package com.bicontest.egg;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
-
-import android.widget.RelativeLayout;
-import androidx.appcompat.widget.Toolbar;
-
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
     private FoldersAdapter mFolderRecyclerViewAdapter;
 
     // 설정 버튼
-    ImageView setting_btn;
+//    ImageView setting_btn;
     Toolbar search_bar;
 
     @Override
@@ -58,8 +58,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(search_bar);
 
 
-        setting_btn = (ImageView) findViewById(R.id.setting_btn);
-        setting_btn.setOnClickListener(new MainActivity.settingClickListener());
+//        setting_btn = (ImageView) findViewById(R.id.setting_btn);
+//        setting_btn.setOnClickListener(new MainActivity.settingClickListener());
 
         firstInit();
 
@@ -90,6 +90,61 @@ public class MainActivity extends AppCompatActivity {
         mFolderRecyclerView.setLayoutManager(new GridLayoutManager(this, 2)); // 수직 리스트
         //mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false)); // 수평 리스트
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        //search_menu.xml 등록
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_toolbar, menu);
+        MenuItem mSearch = menu.findItem(R.id.menu_search);
+
+        //메뉴 아이콘 클릭했을 시 확장, 취소했을 시 축소
+//        mSearch.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
+//            @Override
+//            public boolean onMenuItemActionExpand(MenuItem item) {
+//                TextView text=(TextView)findViewById(R.id.txtstatus);
+//                text.setText("현재 상태 : 확장됨");
+//                return true;
+//            }
+//
+//            @Override
+//            public boolean onMenuItemActionCollapse(MenuItem item) {
+//                TextView text=(TextView)findViewById(R.id.txtstatus);
+//                text.setText("현재 상태 : 축소됨");
+//                return true;
+//            }
+//        });
+
+
+        //menuItem을 이용해서 SearchView 변수 생성
+        SearchView sv=(SearchView)mSearch.getActionView();
+        //확인버튼 활성화
+        sv.setSubmitButtonEnabled(true);
+
+        //SearchView의 검색 이벤트
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            //검색버튼을 눌렀을 경우
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+//                TextView text = (TextView)findViewById(R.id.txtresult);
+//                text.setText(query + "를 검색합니다.");
+                return true;
+            }
+
+            //텍스트가 바뀔때마다 호출
+            @Override
+            public boolean onQueryTextChange(String newText) {
+//                TextView text = (TextView)findViewById(R.id.txtsearch);
+//                text.setText("검색식 : "+newText);
+                return true;
+            }
+        });
+        return true;
+    }
+
+
 
     // 추천 단어 리스트, 폴더 리스트에 필요한 부분
     private void firstInit(){
